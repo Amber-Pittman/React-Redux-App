6a6a2dfb82c3097b0d3e419cfbe5fd952b1b5a22
Amber-Pittman <amber@amberpittman.com>
2019-10-27 18:00:41 -0400

    Trying to get a new activity after clicking btn again

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -9,9 +9,10 @@ import Activity from "./components/Activity";
 import ActivityButton from "./components/ActivityButton";
 
 function App(props) {
+  
   useEffect(() => {
     props.getActivity();
-  }, []) // putting in props inside the brackets makes it run a continuous loop - Don't do that
+  }, []) // putting in `props` inside the brackets makes it run a continuous loop - Don't do that
          // also removing the brackets creates same issue
 
   const handleClick = e => {
