e3d96bbe9b2362e28fa257052fab7011ccd1beae
Amber-Pittman <amber@amberpittman.com>
2019-10-27 16:39:52 -0400

    Updated reducer and added action file

--- react-redux-app/src/reducers/reducer.js
+++ react-redux-app/src/reducers/reducer.js
@@ -1,6 +1,42 @@
-export function reducer() {
-    return {
-      title: 'Hello world! I\'m in the Redux store!',
+import { GET_ACTIVITY, GET_ACTIVITY_SUCCESS, GET_ACTIVITY_ERROR } from "";
+
+export const initialState = {
+    activity: null,
+    isLoading: false,
+    error: null
+}
+
+export function reducer(state = initialState, action) {
+    switch(action.type) {
+        case GET_ACTIVITY:
+        return {
+            ...state,
+            isLoading: true
+        }
+        case GET_ACTIVITY_SUCCESS:
+            return {
+                ...state,
+                activity: action.payload,
+                isLoading: false
+            }
+        case GET_ACTIVITY_ERROR:
+            return {
+                ...state,
+                error: action.payload,
+                isLoading: false
+            }
+        default:
+            return state;
     }
   }
   
+
+//   {
+//     "activity": "Learn Express.js",
+//     "accessibility": 0.25,
+//     "type": "education",
+//     "participants": 1,
+//     "price": 0.1,
+//     "link": "https://expressjs.com/",
+//     "key": "3943506"
+//   } 
